2ecf6ee0059b0cb3deb7736c8e259414a2c6930b
pipob <pipob.piya@gmail.com>
2020-12-17 18:36:41 +0700

    1 file modified, 1 added

---
+++ src/init-firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1i2el7AZSTKFzNJ14Oh1HXKlj7vKSN4k",
+  authDomain: "smn-react.firebaseapp.com",
+  projectId: "smn-react",
+  storageBucket: "smn-react.appspot.com",
+  messagingSenderId: "138396078243",
+  appId: "1:138396078243:web:120ae3ee13196a6ce4ef16"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+alert('hello')
